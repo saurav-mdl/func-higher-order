cd91805fadb67ba9cfc94b4c5ab0571d74c30f07
Saurav Sahu <sauravsahu.12@live.com>
2018-01-16 10:11:44 +0530

    Test passed for running increment with argument as negative value.

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ let repeat = require('./repeat');
 let counter = 0;
 function increment() { counter++; }
 
+counter = 0;
 repeat(increment, 3);
 console.log("Test for running 'increment' function 3 times:", counter === 3);
+
+counter = 0;
+repeat(increment, -2);
+console.log("test for running 'increment' function with -2 as argument:", counter === 0);
